97299f3d6940d26146f4b2d3f8362a878b283d37
Carter Allen <CarterA@opt-6.com>
2012-04-21 10:14:42 -0600

    Modified the Plot.py file to graph the Geiger data.

--- Plot.py
+++ Plot.py
@@ -14,9 +14,17 @@ import matplotlib.pyplot as plt
 # 0 , 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  , 10   , 11   , 12    , 13 , 14 , 15
 # ms,accX,accY,accZ,magX,magY,magZ,gyrX,gyrY,gyrZ,gyrTem,barTem,barPres,tem1,tem2,hum
 
-f = csv.reader(open('Data/Altitude.csv','r'))
-data = data = [line for line in f if len(line) == 16]
+f = csv.reader(open('Data/Flight.txt','r'))
+gf = csv.reader(open('Data/Geiger-Modified.txt', 'r'))
+data = [line for line in f if len(line) == 16]
 data = zip(*data)
+geigerData = [line for line in gf]
+geigerData = zip(*geigerData)
+geigerX = list()
+index = 0
+for line in geigerData[0]:
+    geigerX.insert(index, float(index + 1)/60)
+    index += 1
 
 # Convert ms to seconds for display purposes
 data[0] = [float(t) / 60000 for t in data[0]]
@@ -30,7 +38,7 @@ temPlot.plot(data[0], data[10], 'b-', label='Gyroscope Temperature')
 temPlot.plot(data[0], data[11], 'g-', label='Barometer Temperature')
 temPlot.plot(data[0], data[13], 'r-', label='External Temperature')
 temPlot.plot(data[0], data[14], 'c-', label='Internal Temperature')
-temPlot.legend()
+#temPlot.legend()
 temPlot.set_title('Temperature')
 temPlot.set_xlabel(time_label)
 temPlot.set_ylabel(u'Temperature (‌‌\u00B0C)')
@@ -46,8 +54,8 @@ humPlot.set_ylabel('Relative Humidity (%)')
 # Pressure/Altitude Graph
 pressurePlot = fig.add_subplot(223)
 pressurePlotFormatter = matplotlib.ticker.ScalarFormatter(useOffset = False)
-seaLevelPressure = 101320;
-altitude = [(44330 * (1.0 - pow(float(a)/seaLevelPressure,0.1903)))/1000 for a in data[12]]
+seaLevelPressure = 999.0;
+altitude = [(1.0 - pow(((float(a)/100)/seaLevelPressure), 0.190284)) * 145366.45 for a in data[12]]
 data[12] = [float(p) / 101325 for p in data[12]] # pascals -> atm
 pressurePlot.plot(data[0], data[12], 'b-')
 pressurePlot.set_xlabel(time_label)
@@ -57,19 +65,30 @@ pressurePlot.set_title('Pressure & Altitude')
 altitudePlot = pressurePlot.twinx()
 #data[13] = [float(a) / 1000 for a in data[13]]
 altitudePlot.plot(data[0], altitude, 'r-')
-altitudePlot.set_ylabel('Altitude (kilometers)')
+altitudePlot.set_ylabel('Altitude (feet)')
 altitudePlot.yaxis.set_major_formatter(pressurePlotFormatter)
 for label in pressurePlot.get_yticklabels(): label.set_color('b')
 for label in altitudePlot.get_yticklabels(): label.set_color('r')
 
+# Geiger counter graph
+geigerPlot = fig.add_subplot(224)
+geigerPlot.plot(geigerX, geigerData[1])
+geigerPlot.set_title('Geiger Counter Readings')
+geigerPlot.set_xlabel(time_label)
+geigerPlot.set_ylabel('Counts per second')
+
 # Accelerometer Graph
-accPlot = fig.add_subplot(224)
-accPlot.plot(data[0], data[1], 'b-', label='X Axis')
-accPlot.plot(data[0], data[2], 'g-', label='Y Axis')
-accPlot.plot(data[0], data[3], 'r-', label='Z Axis')
-accPlot.legend()
-accPlot.set_title('Acceleration')
-accPlot.set_xlabel(time_label)
-accPlot.set_ylabel('Accleration (G forces)')
+#accPlot = fig.add_subplot(224)
+#accPlot.plot(data[0], data[1], 'b-', label='X Axis')
+#accPlot.plot(data[0], data[2], 'g-', label='Y Axis')
+#accPlot.plot(data[0], data[3], 'r-', label='Z Axis')
+#accPlot.legend()
+#accPlot.set_title('Acceleration')
+#accPlot.set_xlabel(time_label)
+#accPlot.set_ylabel('Accleration (G forces)')
 
 plt.show()
+#from matplotlib.backends.backend_pdf import PdfPages
+#pp = PdfPages('multipage.pdf')
+#pp.savefig()
+#pp.close()
