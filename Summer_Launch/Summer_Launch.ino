125de2505443a3e800dd3102ca458307741b005c
Carter Allen <CarterA@opt-6.com>
2013-08-02 16:25:40 -0600

    Added the air sample code used for the Summer 2013 launch.

---
+++ Summer_Launch/Summer_Launch.ino
@@ -0,0 +1,66 @@
+//
+//  Flight Computer
+//  CCD Curiosity Balloon Satellite Project
+//  Copyright 2013 Carter Allen
+//
+
+#include <Wire.h>
+#include <BMP085.h>
+
+BMP085 baro;
+
+long Baud = 115200;
+long AirSampleLength = 120000;
+int  PumpPin = 9;
+int  TakenAirSample = 0;
+
+void setupBarometer(void);
+
+void setup() {
+  pinMode(PumpPin, OUTPUT);
+  Serial.begin(Baud);
+  delay(4000);
+  Serial.println();
+  Serial.println("-+-+-+- Oz -+-+-+-");
+  Wire.begin();
+  setupBarometer();
+  delay(1000);
+}
+
+void loop() {
+  
+  writeBarometerData();
+  
+  Serial.println();
+  
+}
+
+void setupBarometer() {
+  baro.begin();
+}
+
+void writeBarometerData() {
+  Serial.print(baro.readTemperature());
+  Serial.print(",");
+  float pressure = baro.readPressure();
+  Serial.print(pressure);
+  
+  if (!TakenAirSample /*&& pressure <= 3119.0*/) {
+    collectAirSample();
+    TakenAirSample = 1;
+  }
+  
+}
+
+void collectAirSample() {
+  Serial.println();
+  Serial.print("Beginning air sample.");
+  digitalWrite(PumpPin, HIGH);
+  delay(100);
+  digitalWrite(PumpPin, LOW);
+  delay(120000);
+  Serial.println("Ending air sample.");
+  digitalWrite(PumpPin, HIGH);
+  delay(100);
+  digitalWrite(PumpPin, LOW);
+}
